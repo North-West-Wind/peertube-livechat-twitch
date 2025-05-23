7eeb24f9033e2b695276c323f6c18a1a7fba763a
North-West-Wind <wsyn148@gmail.com>
2025-05-23 09:19:51 +0800

    p2t ws server

---
+++ peertube-to-twitch/server/src/index.ts
@@ -0,0 +1,93 @@
+import "dotenv/config";
+import { Message, PeerTubeXMPPClient } from "peertube-livechat-xmpp";
+import { WebSocketServer } from "ws";
+
+let port = parseInt(process.env.PORT || "8180");
+if (isNaN(port)) port = 8180;
+
+const wsKeepAlive = parseInt(process.env.WS_KEEP_ALIVE || "0");
+
+const xmppClients = new Map<string, { connected: number, client: PeerTubeXMPPClient, timeout?: NodeJS.Timeout }>(); // concat of instance and roomid -> client
+
+const server = new WebSocketServer({ port });
+server.on("connection", socket => {
+	let tcpKeepAlive = setTimeout(() => {
+		socket.close();
+	}, 60000);
+
+	let key: string | undefined;
+	let xmpp: PeerTubeXMPPClient | undefined;
+
+	const oldMessageListener = (message: Message) => {
+		const author = message.author();
+		if (!author) return;
+		socket.send(`old ${encodeURIComponent(author.nickname)} ${encodeURIComponent(message.body)}`);
+	};
+
+	const newMessageListener = (message: Message) => {
+		const author = message.author();
+		if (!author) return;
+		socket.send(`new ${encodeURIComponent(author.nickname)} ${encodeURIComponent(message.body)}`);
+	};
+
+	const disconnect = () => {
+		if (key && xmppClients.has(key)) {
+			const immKey = key;
+			const details = xmppClients.get(immKey)!;
+			details.connected--;
+			if (!isNaN(wsKeepAlive) && wsKeepAlive > 0)
+				details.timeout = setTimeout(() => {
+					xmppClients.delete(immKey);
+				}, wsKeepAlive);
+		}
+		xmpp?.removeListener("oldMessage", oldMessageListener);
+		xmpp?.removeListener("message", newMessageListener);
+		xmpp = undefined;
+		key = undefined;
+	};
+
+	socket.on("message", data => {
+		const args = data.toString().split("/\s+/");
+		const first = args.shift()!;
+		switch (first) {
+			case "ping": {
+				tcpKeepAlive.refresh();
+				socket.send("pong");
+				break;
+			}
+			case "con": {
+				if (args.length < 2) socket.send("err no-args");
+				else {
+					const [instance, roomId] = args;
+					key = `${roomId}@${instance}`;
+					// Obtain suitable XMPP client
+					if (xmppClients.has(key)) {
+						const details = xmppClients.get(key)!;
+						details.connected++;
+						if (details.timeout) {
+							clearTimeout(details.timeout);
+							details.timeout = undefined;
+						}
+						xmpp = details.client;
+					} else {
+						xmpp = new PeerTubeXMPPClient(instance, roomId, { nickname: "Twitch Bridge #" + Math.ceil(Math.random() * 20) });
+						xmppClients.set(key, { connected: 1, client: xmpp });
+					}
+					// Setup events to redirect to WS
+					xmpp.on("oldMessage", oldMessageListener);
+					xmpp.on("message", newMessageListener);
+				}
+				break;
+			}
+			case "dis": {
+				disconnect();
+				socket.send("dis");
+				break;
+			}
+		}
+	});
+
+	socket.on("close", () => {
+		disconnect();
+	});
+});
